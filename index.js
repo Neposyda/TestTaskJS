209b06871d70a7645a7844da826c2c4e3e1c8573
Neposyda <v.guard333@gmail.com>
2025-03-18 15:20:25 +0200

    refactored code

--- index.js
+++ index.js
@@ -14,37 +14,63 @@ Mexico City,8874724,1486,5974,Mexico
 London,8673713,1572,5431,United Kingdom
 New York City,8537673,784,10892,United States
 Bangkok,8280925,1569,5279,Thailand`;
+// let timeStart = Date.now();
+// if (data) {
+//     const lines = data.split('\n');
+//     lines.pop();
+//     const table = [];
+//     let first = true;
+//     let max = 0;
+//     for (const line of lines) {
+//         if (first) {
+//             first = false;
+//         } else {
+//             const cells = line.split(',');
+//             const d = parseInt(cells[3]);
+//             if (d > max) max = d;
+//             table.push([cells[0], cells[1], cells[2], cells[3], cells[4]]);
+//         }
+//     }
+//     for (const row of table) {
+//         const a = Math.round((row[3] * 100) / max);
+//         row.push(a.toString());
+//     }
+//     table.sort((r1, r2) => r2[5] - r1[5]);
+//     for (const row of table) {
+//         let s = row[0].padEnd(18);
+//         s += row[1].padStart(10);
+//         s += row[2].padStart(8);
+//         s += row[3].padStart(8);
+//         s += row[4].padStart(18);
+//         s += row[5].padStart(6);
+//         console.log(s);
+//     }
+// }
+// console.log(`worked for ${Date.now() - timeStart} ms`);
 
-if (data) {
-    const lines = data.split('\n');
-    lines.pop();
-    const table = [];
-    let first = true;
-    let max = 0;
-    for (const line of lines) {
-        if (first) {
-            first = false;
-        } else {
-            const cells = line.split(',');
-            const d = parseInt(cells[3]);
-            if (d > max) max = d;
-            table.push([cells[0], cells[1], cells[2], cells[3], cells[4]]);
-        }
-    }
-    for (const row of table) {
-        const a = Math.round((row[3] * 100) / max);
-        row.push(a.toString());
-    }
-    table.sort((r1, r2) => r2[5] - r1[5]);
-    for (const row of table) {
-        let s = row[0].padEnd(18);
-        s += row[1].padStart(10);
-        s += row[2].padStart(8);
-        s += row[3].padStart(8);
-        s += row[4].padStart(18);
-        s += row[5].padStart(6);
-        console.log(s);
-    }
-}
+let timeStart = Date.now();
+const [names, ...dataRows] = data.split("\n").map(line => line.split(","));
+
+dataTable = dataRows.reduce((acc, row) => {
+        const cityRow = names.reduce( (acc, name, i ) => {
+                acc[name] = row[i];
+                return acc;
+        }, {});
+        return [...acc, cityRow];
+    }, []);
+
+const maxValue = Math.max(...dataTable.map(city => parseInt(city['density'])));
+
+dataTable.forEach((city) => {
+        city['percent'] = Math.round( city['density']/maxValue*100).toString();
+});
+
+dataTable.sort((a,b)=>b.percent-a.percent).forEach((city) => {
+    console.log(
+        `${city['city'].padEnd(18)}${city['population'].padStart(10)}${city['area'].padStart(8)}${city['density'].padStart(8)}${city['country'].padStart(18)}${city['percent'].padStart(6)}`
+    )
+});
+
+console.log(`worked for ${Date.now() - timeStart} ms`);
 // </code>
 // </pre>
